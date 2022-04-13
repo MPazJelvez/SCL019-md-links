6e3fd6c98cb55e887a3b36328c86d6e37aa304e1
María Paz Jélvez Herrera <mpazjelvez@gmail.com>
2022-04-13 12:39:15 -0400

    colors

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 const start = require('./start.js');
-// const chalk = require('chalk');
+const colors = require('colors');
 
+process.stdout.write(' \n')
 process.stdout.write('✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨\n')
-process.stdout.write('✨ ------ BIENVENID@ A MD-LINKS!! ------✨ \n')
+process.stdout.write(colors.rainbow('✨ ------ BIENVENID@ A MD-LINKS!! ------✨ \n'));
 process.stdout.write('✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨\n')
+process.stdout.write(' \n')
 
 let pathLink ="";
 
-const path1 = path.resolve(pathLink)
-
+//verifica si existe la ruta
 const exists = start.existenceVerification;
 
-process.stdout.write('Ingresa la ruta que deseas analizar:')
+//convierte la ruta en absoluta
+const path1 = path.resolve(pathLink)
 
+//solicita que el usuario agregue la ruta a analizar
+process.stdout.write(colors.cyan('Ingresa la ruta que deseas analizar:'));
+
+//input para que el usuario escriba la ruta
 process.stdin.on('data', function(data){
   
+  //comvierte la data en string y el trim le quita los espacios y "enters" innecesarios
   pathLink = data.toString().trim();
 
-  process.stdout.write(`Ruta: ${pathLink} \n`);
+  //Imprime el nombre de la ruta a través de una plantilla
+  process.stdout.write(colors.magenta(`La ruta es: ${pathLink} \n`));
+
+  //si existe....
 if(exists(pathLink)){
-  process.stdout.write('Existe!😊 \n');
-  process.stdout.write('Trasformando la ruta en absoluta... \n');
-  process.stdout.write(path1 + '\n');
-}else if(!exists(pathLink)){
-  process.stdout.write('No existe!😟 \n')} 
+  process.stdout.write(colors.green('¡EXISTE!😊 \n')); //se imprime esto
+  process.stdout.write(colors.yellow('Trasformando la ruta en absoluta... \n')); //esto
+  process.stdout.write(colors.green(path1 + '\n')); //y finalmente imprimimos la ruta absoluta que logramos con resolve
+}else if(!exists(pathLink)){ //de lo contrario
+  process.stdout.write(colors.red('No existe!😟 \n'))}  //nos indica que no existe
 
 // process.stdout.write('Deseas continuar y analizar otra ruta?  \n')
+
 // exit and stop process
   process.exit()
-})
+});
